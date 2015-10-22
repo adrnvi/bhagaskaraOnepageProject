7a4b6215fdd7bff69d98c8e4e4ebb5424ac68181
adrnvi <adrian.senecki@gmail.com>
2015-10-23 00:33:08 +0200

    added new photos and corrected paddings/margins

--- js/script.js
+++ js/script.js
@@ -5,50 +5,48 @@
 $(document).ready(function(){
 
 
-    var services = $(".services").on("mouseenter", function(){
-       $(this).css("backgroundImage", "url(img/active.png)");
-    });
-    var services = $(".services").on("mouseleave", function(){
-        $(this).css("backgroundImage", "url(img/border.png)");
-    });
 
-    var portfolio = $(".portfolio").on("mouseenter", function(){
-        $(this).css("backgroundImage", "url(img/active.png)");
-    });
-    var portfolio = $(".portfolio").on("mouseleave", function(){
-        $(this).css("backgroundImage", "url(img/border.png)");
-    });
+            var services = $(".services").on("mouseenter", function(){
+                $(this).css("backgroundImage", "url(img/active.png)");
+            });
+            var services = $(".services").on("mouseleave", function(){
+                $(this).css("backgroundImage", "url(img/border.png)");
+            });
 
-    var about = $(".about").on("mouseenter", function(){
-        $(this).css("backgroundImage", "url(img/active.png)");
-    });
-    var about = $(".about").on("mouseleave", function(){
-        $(this).css("backgroundImage", "url(img/border.png)");
-    });
+            var portfolio = $(".portfolio").on("mouseenter", function(){
+                $(this).css("backgroundImage", "url(img/active.png)");
+            });
+            var portfolio = $(".portfolio").on("mouseleave", function(){
+                $(this).css("backgroundImage", "url(img/border.png)");
+            });
 
-    var team = $(".team").on("mouseenter", function(){
-        $(this).css("backgroundImage", "url(img/active.png)");
-    });
-    var team = $(".team").on("mouseleave", function(){
-        $(this).css("backgroundImage", "url(img/border.png)");
-    });
+            var about = $(".about").on("mouseenter", function(){
+                $(this).css("backgroundImage", "url(img/active.png)");
+            });
+            var about = $(".about").on("mouseleave", function(){
+                $(this).css("backgroundImage", "url(img/border.png)");
+            });
 
-    var blog = $(".blog").on("mouseenter", function(){
-        $(this).css("backgroundImage", "url(img/active.png)");
-    });
-    var blog = $(".blog").on("mouseleave", function(){
-        $(this).css("backgroundImage", "url(img/border.png)");
-    });
+            var team = $(".team").on("mouseenter", function(){
+                $(this).css("backgroundImage", "url(img/active.png)");
+            });
+            var team = $(".team").on("mouseleave", function(){
+                $(this).css("backgroundImage", "url(img/border.png)");
+            });
 
-    var contact = $(".contact").on("mouseenter", function(){
-        $(this).css("backgroundImage", "url(img/active.png)");
-    });
-    var contact = $(".contact").on("mouseleave", function(){
-        $(this).css("backgroundImage", "url(img/border.png)");
-    });
+            var blog = $(".blog").on("mouseenter", function(){
+                $(this).css("backgroundImage", "url(img/active.png)");
+            });
+            var blog = $(".blog").on("mouseleave", function(){
+                $(this).css("backgroundImage", "url(img/border.png)");
+            });
 
-
-
+            var contact = $(".contact").on("mouseenter", function(){
+                $(this).css("backgroundImage", "url(img/active.png)");
+            });
+            var contact = $(".contact").on("mouseleave", function(){
+                $(this).css("backgroundImage", "url(img/border.png)");
+            });
 
 
 });

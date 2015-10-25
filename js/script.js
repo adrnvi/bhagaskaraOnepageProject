43dab3e273e56c7dfa76b365fede0c63d441a94d
adrnvi <adrian.senecki@gmail.com>
2015-10-25 08:45:22 +0100

    further styling and hover/anchor features added to the project

--- js/script.js
+++ js/script.js
@@ -2,51 +2,68 @@
  * Created by AdrianS on 2015-10-21.
  */
 
-$(document).ready(function(){
+
+$(document).ready(function() {
+
+//replace menu imgs
+
+ var menuHover = $(".menuHover").on("mouseenter", function(){
+     $(this).css("backgroundImage", "url(img/active.png)");
+ });
+ menuHover.on("mouseleave", function(){
+     $(this).css("backgroundImage", "url(img/border.png)");
+ });
+
+//replace features imgs
 
 
 
-            var services = $(".services").on("mouseenter", function(){
-                $(this).css("backgroundImage", "url(img/active.png)");
-            });
-            var services = $(".services").on("mouseleave", function(){
-                $(this).css("backgroundImage", "url(img/border.png)");
-            });
 
-            var portfolio = $(".portfolio").on("mouseenter", function(){
-                $(this).css("backgroundImage", "url(img/active.png)");
-            });
-            var portfolio = $(".portfolio").on("mouseleave", function(){
-                $(this).css("backgroundImage", "url(img/border.png)");
-            });
+//replace arrow img
 
-            var about = $(".about").on("mouseenter", function(){
-                $(this).css("backgroundImage", "url(img/active.png)");
-            });
-            var about = $(".about").on("mouseleave", function(){
-                $(this).css("backgroundImage", "url(img/border.png)");
-            });
+var arrowDown = $(".arrowDown").on("mouseenter", function(){
+    $(this).css("backgroundImage", "url(img/arrowActive.png)");
+});
+arrowDown.on("mouseleave", function(){
+    $(this).css("backgroundImage", "url(img/arrowDown.png)");
+});
 
-            var team = $(".team").on("mouseenter", function(){
-                $(this).css("backgroundImage", "url(img/active.png)");
-            });
-            var team = $(".team").on("mouseleave", function(){
-                $(this).css("backgroundImage", "url(img/border.png)");
-            });
 
-            var blog = $(".blog").on("mouseenter", function(){
-                $(this).css("backgroundImage", "url(img/active.png)");
-            });
-            var blog = $(".blog").on("mouseleave", function(){
-                $(this).css("backgroundImage", "url(img/border.png)");
-            });
+//animate anchors
 
-            var contact = $(".contact").on("mouseenter", function(){
-                $(this).css("backgroundImage", "url(img/active.png)");
-            });
-            var contact = $(".contact").on("mouseleave", function(){
-                $(this).css("backgroundImage", "url(img/border.png)");
-            });
+$('a[href*=#mainNav]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
+
+$('a[href*=#services]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+});
+
+$('a[href*=#portfolio]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+})
+
+$('a[href*=#about]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+})
+
+$('a[href*=#team]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+})
+
+$('a[href*=#contact]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1500);
+})
+
+
 
 
 });
+

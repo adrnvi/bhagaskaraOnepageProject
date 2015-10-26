37270e465eef912cfecfb92b63dad9fad3f4d641
adrnvi <adrian.senecki@gmail.com>
2015-10-26 15:43:06 +0100

    added new script structure and background attachment

--- js/script.js
+++ js/script.js
@@ -1,207 +1,211 @@
 /**
  * Created by AdrianS on 2015-10-21.
  */
-
-
 $(document).ready(function() {
 
-//replace menu imgs
+var Application = function() {
 
- var menuHover = $(".menuHover").on("mouseenter", function(){
-     $(this).css("backgroundImage", "url(img/active.png)");
- });
- menuHover.on("mouseleave", function(){
-     $(this).css("backgroundImage", "url(img/border.png)");
- });
+    function replaceImg() {
+        var menuHover = $(".menuHover").on("mouseenter", function(){
+            $(this).css("backgroundImage", "url(img/active.png)");
+        });
+        menuHover.on("mouseleave", function(){
+            $(this).css("backgroundImage", "url(img/border.png)");
+        });
+    }
 
-//replace features imgs
+    function replaceFeatureImg() {
+        var diamondImg = $(".diamondImg").on("mouseenter", function(){
+            $(this).css("backgroundImage", "url(img/diamondImgHover.png)");
+        });
+        diamondImg.on("mouseleave", function(){
+            $(this).css("backgroundImage", "url(img/diamondImg.png)");
+        });
 
-var diamondImg = $(".diamondImg").on("mouseenter", function(){
-    $(this).css("backgroundImage", "url(img/diamondImgHover.png)");
-});
-diamondImg = $(".diamondImg").on("mouseleave", function(){
-    $(this).css("backgroundImage", "url(img/diamondImg.png)");
-});
+        var binImg = $(".binImg").on("mouseenter", function(){
+            $(this).css("backgroundImage", "url(img/binImgHover.png)");
+        });
+        binImg.on("mouseleave", function(){
+            $(this).css("backgroundImage", "url(img/binImg.png)");
+        });
 
-var binImg = $(".binImg").on("mouseenter", function(){
-    $(this).css("backgroundImage", "url(img/binImgHover.png)");
-});
-binImg = $(".binImg").on("mouseleave", function(){
-    $(this).css("backgroundImage", "url(img/binImg.png)");
-});
+        var phoneImg = $(".phoneImg").on("mouseenter", function(){
+            $(this).css("backgroundImage", "url(img/phoneImgHover.png)");
+        });
+        phoneImg.on("mouseleave", function(){
+            $(this).css("backgroundImage", "url(img/phoneImg.png)");
+        });
+    }
 
-var phoneImg = $(".phoneImg").on("mouseenter", function(){
-    $(this).css("backgroundImage", "url(img/phoneImgHover.png)");
-});
-phoneImg = $(".phoneImg").on("mouseleave", function(){
-    $(this).css("backgroundImage", "url(img/phoneImg.png)");
-});
+    function replaceArrow() {
+        var arrowDown = $(".arrowDown").on("mouseenter", function(){
+            $(this).css("backgroundImage", "url(img/arrowActive.png)");
+        });
+        arrowDown.on("mouseleave", function(){
+            $(this).css("backgroundImage", "url(img/arrowDown.png)");
+        });
+    }
 
-//replace arrow img
+    function animateAnchors() {
+        $('a[href*=#mainNav]').on('click', function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+        });
 
-var arrowDown = $(".arrowDown").on("mouseenter", function(){
-    $(this).css("backgroundImage", "url(img/arrowActive.png)");
-});
-arrowDown.on("mouseleave", function(){
-    $(this).css("backgroundImage", "url(img/arrowDown.png)");
-});
+        $('a[href*=#services]').on('click', function(event){
+            event.preventDefault();
 
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
+        });
 
-//animate anchors
+        $('a[href*=#portfolio]').on('click', function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
+        });
 
-$('a[href*=#mainNav]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-});
+        $('a[href*=#about]').on('click', function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
+        });
 
+        $('a[href*=#team]').on('click', function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
+        });
 
-$('a[href*=#services]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
-});
+        $('a[href*=#contact]').on('click', function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
+        });
 
-$('a[href*=#portfolio]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
-})
+    }
 
-$('a[href*=#about]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
-})
+    function makeSticky() {
+        var menu = $(".mainNav");
+        var menuBar = $(".mainNav .row");
+        var sections = $("section[id]");
+        var links = $(".mainNav a[href]").not(document.getElementById("notblog"));
+        var lastMenuPositionFromTop = 0;
 
-$('a[href*=#team]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
-})
+        $(window).scroll(function(event){
+            if( menu.hasClass("sticky") === false &&
+                $(this).scrollTop() > menu.offset().top && $(window).width() > 900) {
 
-$('a[href*=#contact]').on('click', function(event){
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 2000);
-})
+                lastMenuPositionFromTop = menu.offset().top;
+                menu.addClass("sticky");
+                menuBar.css("padding", "1% 0 0 0");
+            }
 
-// sticky menu
+            if( menu.hasClass("sticky") &&
+                $(this).scrollTop() < lastMenuPositionFromTop){
 
-    var menu = $(".mainNav");
-    var menuBar = $(".mainNav .row");
-    var sections = $("section[id]");
-    var links = $(".mainNav a[href]").not(document.getElementById("notblog"));
-    var lastMenuPositionFromTop = 0;
+                menu.removeClass("sticky");
+                menuBar.css("padding", "5% 0 4% 0");
+            }
 
-    $(window).scroll(function(event){
-        if( menu.hasClass("sticky") === false &&
-            $(this).scrollTop() > menu.offset().top && $(window).width() > 900) {
-
-            lastMenuPositionFromTop = menu.offset().top;
-            menu.addClass("sticky");
-            menuBar.css("padding", "1% 0 0 0");
-        }
-
-        if( menu.hasClass("sticky") &&
-            $(this).scrollTop() < lastMenuPositionFromTop){
-
-            menu.removeClass("sticky");
-            menuBar.css("padding", "5% 0 4% 0");
-        }
-
-        sections.each(function(index){
-            if(index + 1 >= sections.length){
-                if( sections.eq(index).offset().top < $(window).scrollTop()){
-                    links.eq(index).addClass("purple");
+            sections.each(function(index){
+                if(index + 1 >= sections.length){
+                    if( sections.eq(index).offset().top < $(window).scrollTop()){
+                        links.eq(index).addClass("purple");
+                    }
+                    else {
+                        links.eq(index).removeClass("purple");
+                    }
                 }
                 else {
-                    links.eq(index).removeClass("purple");
-                }
-            }
-            else {
-                if( sections.eq(index).offset().top < $(window).scrollTop() &&
-                    sections.eq(index + 1).offset().top > $(window).scrollTop()){
+                    if( sections.eq(index).offset().top < $(window).scrollTop() &&
+                        sections.eq(index + 1).offset().top > $(window).scrollTop()){
 
-                    links.eq(index).addClass("purple");
+                        links.eq(index).addClass("purple");
+                    }
+                    else{
+                        links.eq(index).removeClass("purple");
+                    }
                 }
-                else{
-                    links.eq(index).removeClass("purple");
-                }
-            }
+            });
         });
-    });
+    }
 
+    function makeCarousel() {
+        var leftArrow = $(".leftArrow");
+        var rightArrow = $(".rightArrow");
 
-// simple slider
+        var person1 = $(".person1");
+        var person2 = $(".person2");
+        var person3 = $(".person3");
 
+        var skillset1 = $(".personOneSkills");
+        var skillset2 = $(".personTwoSkills");
+        var skillset3 = $(".personThreeSkills");
 
-    var leftArrow = $(".leftArrow");
-    var rightArrow = $(".rightArrow");
+        var allPersons = $(".allItems");
 
-    var people = $(".person");
+        rightArrow.on("click", function(event){
 
-    var allPersons = $(".allItems");
+            event.preventDefault();
+            var people = $(".person");
 
+            people.eq(2).clone(true).prependTo(allPersons);
+            people.eq(2).remove();
 
-    rightArrow.on("click", function(event){
+            if(people.eq(0).hasClass("person1")) {
+                skillset2.hide(500);
+                skillset3.hide(500);
+                skillset1.show(500);
+            } else if (people.eq(0).hasClass("person2")) {
+                skillset1.hide(500);
+                skillset3.hide(500);
+                skillset2.show(500);
+            } else if (people.eq(0).hasClass("person3")) {
+                skillset1.hide(500);
+                skillset2.hide(500);
+                skillset3.show(500);
+            }
 
-        event.preventDefault();
+        });
 
+        leftArrow.on("click", function(event){
 
-        people.eq(2).clone(true).prependTo(allPersons);
-        console.log(people.eq(2))
-        people.eq(2).remove();
+            event.preventDefault();
+            var people = $(".person");
 
-        /*$.each(people, function(index){
+            people.eq(0).clone(true).appendTo(allPersons);
+            people.eq(0).remove();
 
-        })*/
+            if(people.eq(2).hasClass("person1")) {
+                skillset2.hide(500);
+                skillset3.hide(500);
+                skillset1.show(500);
+            } else if (people.eq(2).hasClass("person2")) {
+                skillset1.hide(500);
+                skillset3.hide(500);
+                skillset2.show(500);
+            } else if (people.eq(2).hasClass("person3")) {
+                skillset1.hide(500);
+                skillset2.hide(500);
+                skillset3.show(500);
+            }
 
+        });
 
-    });
+    }
 
-    leftArrow.on("click", function(event){
+    return {
+        replaceImg: replaceImg,
+        replaceFeatureImg: replaceFeatureImg,
+        replaceArrow: replaceArrow,
+        animateAnchors: animateAnchors,
+        makeSticky: makeSticky,
+        makeCarousel: makeCarousel
+    }
+};
 
-        event.preventDefault();
+var app = new Application();
 
-        people.eq(0).clone(true).appendTo(allPersons);
-        people.eq(0).remove();
-
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    var images = $(".ourTeam li");
-    var visibleImage = 0;
-    images.eq(visibleImage).show();
-
-    $(".rightArrow").click(function(){
-        images.eq(visibleImage).hide(0);
-        visibleImage++;
-        if(visibleImage >= images.length){
-            visibleImage = 0;
-        }
-        images.eq(visibleImage).show(0);
-    });
-
-    $(".leftArrow").click(function(){
-        images.eq(visibleImage).hide(0);
-        visibleImage--;
-        if(visibleImage < 0){
-            visibleImage = images.length-1;
-        }
-        images.eq(visibleImage).show(0);
-    });
-*/
-
-// carrousel slider
-
-
-
+app.replaceImg();
+app.replaceFeatureImg();
+app.replaceArrow();
+app.animateAnchors();
+app.makeSticky();
+app.makeCarousel();
 });
-
